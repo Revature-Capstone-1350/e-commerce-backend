379e220955e4c73d009c9378a25a3b0bd44252a1
ZacharyCooremans <77285580+ZacharyCooremans@users.noreply.github.com>
2022-07-18 09:59:22 -0500

    Fixed comments on the ProductReviewService
    Co-Authored-by: trueHiddenLotus <jermaine.roberts19@yahoo.com>
    Co-Authored-by: isaachrr4 <isaachrr4@gmail.com>
    Co-Authored-by: mbmcmullan <michael794@revature.net>
    Co-Authored-by: almustafaj <almustafa830@revature.net

--- src/main/java/com/revature/services/ProductReviewService.java
+++ src/main/java/com/revature/services/ProductReviewService.java
@@ -16,19 +16,37 @@ public class ProductReviewService {
         this.productReviewRepository = productReviewRepository;
     }
 
+    /**
+     * Makes a list of all products
+     * @return a list of all products
+     */
     public List<ProductReview> findAll() {
-        return productReviewRepository.findAll(); // Return a list of all reviews
+        return productReviewRepository.findAll();
     }
 
+    /**
+     * Find a product review by its id
+     * @param id a integer of the id you want
+     * @return Return a product review by their id
+     */
     public Optional<ProductReview> findById(int id) {
-        return productReviewRepository.findById(id); // Return a product review by their id
+        return productReviewRepository.findById(id);
     }
 
+    /**
+     * Find all users reviews by product id
+     * @param id the id of the product we want
+     * @return // Return a List of all reviews for that product
+     */
     public List<ProductReview> findAllByProductId(int id) {
-        return productReviewRepository.findByProductId(id); // Return a List of all reviews for that product
+        return productReviewRepository.findByProductId(id);
     }
 
+    /**
+     * Deletes a review by its id
+     * @param id the id of the review we want deleted
+     */
     public void delete(int id) {
         productReviewRepository.deleteById(id);
-    } // delete product review by id
+    }
 }
