9acc64c00136ae4a727a3922273a1b4bb66fbd46
ZacharyCooremans <77285580+ZacharyCooremans@users.noreply.github.com>
2022-07-18 10:35:40 -0500

    trying to get to build

--- src/main/java/com/revature/repositories/ProductReviewRepository.java
+++ src/main/java/com/revature/repositories/ProductReviewRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductReviewRepository extends JpaRepository<ProductReview, Integer> {
-    List<ProductReview> findByProductId(int id); //Get the product id so we can search for all reviews for product
+    List<ProductReview> findByProductId(int id); //Get the product id so we can search for all reviews for product.
 }
