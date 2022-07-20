1abe8cabde94ecb1f83ac577d4a7583a108ee916
unknown <josiahsparks78@gmail.com>
2022-07-20 14:46:59 -0500

    Create review + integration tests
    Co-Authored-by: trueHiddenLotus <jermaine.roberts19@yahoo.com>
    Co-Authored-by: isaachrr4 <isaachrr4@gmail.com>
    Co-Authored-by: mbmcmullan <michael794@revature.net>
    Co-Authored-by: almustafaj <almustafa830@revature.net>
    Co-Authored-by: ZacharyCooremans <zcooremans@gmail.com>

--- src/main/java/com/revature/controllers/ProductController.java
+++ src/main/java/com/revature/controllers/ProductController.java
@@ -2,12 +2,15 @@ package com.revature.controllers;
 
 import com.revature.annotations.AdminOnly;
 import com.revature.dtos.ProductInfo;
+import com.revature.dtos.ProductReviewRequest;
 import com.revature.exceptions.NotImplementedException;
 import com.revature.services.ProductService;
+import com.revature.services.ReviewService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -15,9 +18,11 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+    private final ReviewService reviewService;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, ReviewService reviewService) {
         this.productService = productService;
+        this.reviewService = reviewService;
     }
 
     @ResponseStatus(HttpStatus.OK)
@@ -30,6 +35,25 @@ public class ProductController {
     @GetMapping("/rating/{id}")
     public ResponseEntity getProductReviewsById(@PathVariable("id") int id) {
         return productService.findReviewsByProductId(id);
+    }
+
+    /**
+     *
+     * @param token
+     * @param reviewReq
+     * @param productId
+     */
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(
+            path = "/rating/{productId}",
+            consumes = "application/json"
+    )
+    public void addReview(
+            @RequestHeader("Authorization") String token,
+            @RequestBody @Valid ProductReviewRequest reviewReq,
+            @PathVariable("productId") int productId
+    ) {
+        reviewService.postReview(token, reviewReq, productId);
     }
 
     @ResponseStatus(HttpStatus.OK)
